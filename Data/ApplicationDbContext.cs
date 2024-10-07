527ded5da5b7b1db16505c095eac9029f8dae932
Jzulu22x <juanzr1015@gmail.com>
2024-10-07 08:39:55 -0500

    feat: creation of seeders for room_type

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hotel_manager.Models;
+using Hotel_manager.Seeders;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hotel_manager.Data;
 public class ApplicationDbContext : DbContext
 {
-    DbSet<Room_type> Room_types {get; set;}
-    DbSet<Room> Rooms {get; set; }
-    DbSet<Guest> Guests {get; set; }
-    DbSet<Employee> Employees { get; set;}
+    public DbSet<Room_type> Room_types {get; set;}
+    public DbSet<Room> Rooms {get; set; }
+    public DbSet<Guest> Guests {get; set; }
+    public DbSet<Employee> Employees { get; set;}
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        RoomTypeSeeder.Seed(modelBuilder);
+    }
 }
