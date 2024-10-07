bd8202df45cff19aab1fc1d5d33b91bc69577f62
Jzulu22x <juanzr1015@gmail.com>
2024-10-07 07:41:58 -0500

    feat: add the model to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hotel_manager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hotel_manager.Data;
 public class ApplicationDbContext : DbContext
 {
+    DbSet<Room_type> Room_types {get; set;}
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 }
---
+++ Migrations/20241007124034_UpdateCreationTableRoom_types.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCreationTableRoom_types : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "room_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_room_types", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "room_types");
+        }
+    }
+}
