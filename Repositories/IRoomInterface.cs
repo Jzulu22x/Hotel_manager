4e0a02a96dedc8c081ac43ff4415632594e16c63
Jzulu22x <juanzr1015@gmail.com>
2024-10-07 10:11:18 -0500

    feat: create the interface for Room EndPoints

---
+++ Repositories/IRoomInterface.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel_manager.Models;
+
+namespace Hotel_manager.Repositories;
+public interface IRoomInterface
+{
+    Task<IEnumerable<Room>> GetAviable();
+    Task<int> GetTotal();
+    Task<IEnumerable<Room>> GetAll();
+    Task<Room> GetById(int id);
+    Task<IEnumerable<Room>> GetOccupied();
+}
