0ee4e0683fb006561fe1dd9e70f1c930171cb107
Jzulu22x <juanzr1015@gmail.com>
2024-10-07 08:04:09 -0500

    feat: creation of the guest model

---
+++ Models/Guests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_manager.Models;
+
+[Table("guests")]
+public class Guests
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("first_name")]
+    public required string FirstName { get; set; }
+
+    [Column("last_name")]
+    public required string LastName { get; set; }
+
+    [Column("email")]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Column("identidication_number")]
+    public required string IdentificationNumber { get; set; }
+
+    [Column("phone_number")]
+    [Phone]
+    public required string PhoneNumber { get; set; }
+        
+    [Column("birthdate")]
+    public DateTime? Birthdate { get; set; }
+}
